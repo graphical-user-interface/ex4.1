d344560658daebc5f5749d17fe76c02fc8a332ec
thang241190 <thangnguyen24111990@gmail.com>
2021-04-19 22:24:02 +0300

    Finish ex4.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import { Container, Grid, Paper, Typography } from "@material-ui/core"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	paper: {
+		margin: theme.spacing(2),
+		padding: theme.spacing(2, 1),
+		textAlign: "center",
+		color: theme.palette.text.secondary,
+		display: "flex",
+	},
+	text: {
+		border: "1px solid #333",
+		padding: "0 10px",
+	},
+}))
+
+export default function App() {
+	const classes = useStyles()
+	return (
+		<div className={classes.root} contentEditable={true}>
+			<Container maxWidth='sm'>
+				<Grid container>
+					<Paper elevation={2} className={classes.paper}>
+						<Typography className={classes.text} variant='h6'>
+							Hmm kimochi
+						</Typography>
+						<Typography className={classes.text} variant='h6'>
+							Hmm kimochi 2
+						</Typography>
+						<Typography className={classes.text} variant='h6'>
+							Hmm kimochi 3
+						</Typography>
+					</Paper>
+				</Grid>
+			</Container>
+		</div>
+	)
 }
-
-export default App;
